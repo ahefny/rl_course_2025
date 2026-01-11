bbc26764c01b48acc94e1e407b13faae40349ebb
Ahmed Hefny <ahmed.s.hefny@gmail.com>
2026-01-11 08:22:49 -0800

    added REINFORCE

--- experiments/reinforce.py
+++ experiments/reinforce.py
@@ -20,19 +20,6 @@ class PolicyNet(nn.Module):
         return torch.distributions.Categorical(logits=self.net(x))
 
 
-class ValueNet(nn.Module):
-    def __init__(self, obs_dim):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(obs_dim, 64),
-            nn.Tanh(),
-            nn.Linear(64, 1)
-        )
-
-    def forward(self, x):
-        return self.net(x).squeeze(-1)
-
-
 # -----------------------------
 # Utilities
 # -----------------------------
@@ -54,10 +41,7 @@ obs_dim = env.observation_space.shape[0]
 act_dim = env.action_space.n
 
 policy = PolicyNet(obs_dim, act_dim)
-value_fn = ValueNet(obs_dim)
-
 policy_optim = optim.Adam(policy.parameters(), lr=1e-3)
-value_optim = optim.Adam(value_fn.parameters(), lr=1e-2)
 
 gamma = 0.99
 
@@ -86,30 +70,20 @@ for episode in range(100000):
     # Convert to tensors
     states = torch.stack(states)
     actions = torch.stack(actions)
-
-    with torch.no_grad():
-        last_reward = value_fn(torch.tensor(obs)) if truncated else 0.0    
-    returns = compute_returns(rewards + [last_reward], gamma)[..., :-1]
-    # returns = torch.sum(torch.tensor(rewards))    
-
-    # ----- Value loss -----
-    values = value_fn(states)
-    value_loss = ((returns - values) ** 2).mean()
-
-    value_optim.zero_grad()
-    value_loss.backward()
-    value_optim.step()
+    
+    returns = compute_returns(rewards, gamma)
+    # Uncomment to disable the causality trick.
+    # returns = torch.sum(torch.tensor(rewards))
 
     # ----- Policy loss -----
-    advantages = returns - values.detach()
     dist = policy(states)
     log_probs = dist.log_prob(actions)
 
-    policy_loss = -(log_probs * advantages).mean()
+    policy_loss = -(log_probs * returns).mean()
 
     policy_optim.zero_grad()
     policy_loss.backward()
     policy_optim.step()
 
     if episode % 50 == 0:
-        print(f"Episode {episode}, Return: {sum(rewards):.1f}, Policy Loss: {policy_loss:.1f}, Value Loss: {value_loss:.1f}")
+        print(f"Episode {episode}, Return: {sum(rewards):.1f}, Policy Loss: {policy_loss:.1f}")
